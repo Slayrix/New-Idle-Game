36cf2ef2b77df905f2e508e78cad7896ceaf310d
Slayrix <52615772+Slayrix@users.noreply.github.com>
2026-03-13 23:39:45 -0500

    Update main.js

--- Current Version/Game/main.js
+++ Current Version/Game/main.js
@@ -1,8 +1,8 @@
 import Button from "./classes/Button.js";
 import {menus} from "./vars/gameVars.js";
-import {upgrades} from "./vars/upgrades.js";
 import {currencies} from "./vars/gameVars.js";
 import {playerStats} from "./vars/gameVars.js";
+import {upgrades} from "./vars/upgrades.js";
 
 const genEnergyButton = new Button("Gen Energy", menus.menu, () => currencies.energy.addAmount(playerStats.energyGenPerClick));
 
