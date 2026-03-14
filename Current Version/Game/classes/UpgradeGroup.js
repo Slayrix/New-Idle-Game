f40a1d88ca981968ad613c3da655b23e7ee5c7f8
Slayrix <52615772+Slayrix@users.noreply.github.com>
2026-03-14 01:23:26 -0500

    Code improvement

--- Current Version/Game/classes/UpgradeGroup.js
+++ Current Version/Game/classes/UpgradeGroup.js
@@ -1,11 +1,4 @@
-export const helperFunctions = {
-    createElement: (type, textContent = null, className = null) => {
-        const element = document.createElement(type);
-        if (textContent != null) element.textContent = textContent;
-        if (className != null) element.classList.add(className);
-        return element;
-    },
-}
+import {helperFunctions} from "../helperFunctions.js";
 
 export default class UpgradeGroup {
     constructor(displayMenu) {
