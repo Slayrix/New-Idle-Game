564c4df94f66e44f886f4b98001dc697f6e84311
Slayrix <52615772+Slayrix@users.noreply.github.com>
2026-04-08 22:47:48 -0500

    Added big bang button

--- Current Version/Game/main.js
+++ Current Version/Game/main.js
@@ -1,9 +1,18 @@
 import {currencies} from "./vars/gameVars.js";
 import {upgrades} from "./vars/upgrades.js";
 import HoverBox from "./classes/HoverBox.js";
+import Button from "./classes/Button.js";
 import {menus} from "./vars/gameVars.js";
+import {helperFunctions} from "./helperFunctions.js";
 
-//const bigBang = new HoverBox("Big Bang", () => {}, "Start Big Bang", [], menus.mainMenu, 20, 70);
+const container = helperFunctions.createElement("div", null, "defaultContainer");
+const bigBang = new Button("Big Bang", () => {}, null, null, null, "upgradeButton");
+const hoverBox = new HoverBox("Create Big Bang", [], bigBang.buttonVar, container);
+
+container.append(bigBang.buttonVar);
+container.style.left = "100px";
+container.style.top = "100px";
+menus.mainMenu.appendElementToContainer(container);
 
 window.debug = {
     currencies,
